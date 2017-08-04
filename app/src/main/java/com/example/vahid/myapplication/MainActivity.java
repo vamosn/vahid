487ef8d135b853a1f53cb4bf06aab295c2e64e5e
vahid <optimcomm@gmail.com>
2017-08-04 11:51:11 +0430

    change #1

--- app/src/main/java/com/example/vahid/myapplication/MainActivity.java
+++ app/src/main/java/com/example/vahid/myapplication/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(getApplicationContext(),"first push in github",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"first push to github",Toast.LENGTH_LONG).show();
 
     }
 }
