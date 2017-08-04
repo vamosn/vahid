e6e741cd4d400ee12b55e4d6648e44f034ffe730
vahid <optimcomm@gmail.com>
2017-08-04 12:23:35 +0430

    ff

--- app/src/main/java/com/example/vahid/myapplication/MainActivity.java
+++ app/src/main/java/com/example/vahid/myapplication/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(getApplicationContext(),"first push github",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"first pus github",Toast.LENGTH_LONG).show();
 
     }
 }
